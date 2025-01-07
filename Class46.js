e4003324eeeab44a29a0cc71ce374621abbf2029
LahariRavi <rraavi555@gmail.com>
2025-01-07 11:52:06 -0800

    Add files via upload

---
+++ Class46.js
@@ -0,0 +1,4 @@
+function loading() {
+    var name = prompt("Enter your name to register to the techno quiz.")
+    document.getElementById("nameoutput").innerHTML = "Hello " + name + ", Welcome to Techno Learner. Your name is registered here."
+}
